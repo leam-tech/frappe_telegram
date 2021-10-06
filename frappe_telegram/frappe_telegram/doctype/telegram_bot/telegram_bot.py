cd9a0d332c599e3c1953ada11e8c58bb04cc3fd0
Fahim Ali Zain <fahimalizain@gmail.com>
2021-10-07 00:29:39 +0530

    fix: Webhook configs in TelegramBot

--- frappe_telegram/frappe_telegram/doctype/telegram_bot/telegram_bot.py
+++ frappe_telegram/frappe_telegram/doctype/telegram_bot/telegram_bot.py
@@ -8,3 +8,15 @@ from frappe.model.document import Document
 class TelegramBot(Document):
     def autoname(self):
         self.name = self.title.replace(" ", "-")
+
+    def validate(self):
+        self.set_nginx_path()
+
+    def set_nginx_path(self):
+        if self.webhook_nginx_path:
+            return
+
+        if not self.webhook_url:
+            return
+
+        self.webhook_nginx_path = "/" + self.webhook_url.rstrip("/").split("/")[-1]
