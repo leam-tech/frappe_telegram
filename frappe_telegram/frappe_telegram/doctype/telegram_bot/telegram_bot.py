7364ed4cf93cacdc1ca9d661ac3f9cc7c0a03aed
Fahim Ali Zain <fahimalizain@gmail.com>
2021-12-10 07:23:21 +0530

    fix: validate bot api_token

--- frappe_telegram/frappe_telegram/doctype/telegram_bot/telegram_bot.py
+++ frappe_telegram/frappe_telegram/doctype/telegram_bot/telegram_bot.py
@@ -11,6 +11,7 @@ class TelegramBot(Document):
         self.name = self.title.replace(" ", "-")
 
     def validate(self):
+        self.validate_api_token()
         self.set_nginx_path()
 
     def after_insert(self):
@@ -43,3 +44,19 @@ class TelegramBot(Document):
     def mark_as_default(self):
         frappe.db.set_default(DEFAULT_TELEGRAM_BOT_KEY, self.name)
         frappe.msgprint(frappe._(f"Set {self.get('title')} as the default bot for notifications."))
+
+    def validate_api_token(self):
+        if not self.is_new() and not self.has_value_changed("api_token"):
+            return
+
+        from telegram.ext import ExtBot
+        try:
+            bot = ExtBot(
+                token=self.api_token
+            )
+            user = bot.get_me()
+            if not user.is_bot:
+                raise Exception(frappe._("TelegramUser is not a Bot"))
+            self.username = "@" + user.username
+        except BaseException as e:
+            frappe.throw(msg=frappe._("Error with Bot Token: {0}").format(str(e)))
